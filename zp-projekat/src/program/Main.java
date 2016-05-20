8a07eeb9164fb9721b854c1cede26160ee215532
Dimitrije Petrovic <mitap94@yahoo.com>
2016-05-20 15:44:17 +0200

    main funkcija i paketi

--- zp-projekat/src/program/Main.java
+++ zp-projekat/src/program/Main.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package program;
 
 /**
  *
  * @author Mita
  */
-public class Klasa {
-    public static void main(String argv[]) {
+public class Main {
+     public static void main(String argv[]) {
         
         System.out.println("Test");
         // Test2.
