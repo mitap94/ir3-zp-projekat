a2d0ccfc92a68e42a1db4590784617bebb9b6da4
uros <uros@uros-velocity-micro>
2016-05-20 14:58:58 +0200

    Mnogo me nerviraju fajlovi koji pocinju malim slovom.

---
+++ zp-projekat/src/Klasa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mita
+ */
+public class Klasa {
+    public static void main(String argv[]) {
+        
+        System.out.println("Test");
+        
+    }
+}
