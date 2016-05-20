727abc5d5d373806ce5979de09b2b64655313c8c
uros <uros@uros-velocity-micro>
2016-05-20 22:39:31 +0200

    Dodata klasa za greske za GUI.

---
+++ zp-projekat/src/gui/Errors.java
@@ -0,0 +1,13 @@
+package gui;
+
+/**
+ * Error constants for GUI.
+ * 
+ * @author uros
+ */
+public class Errors {
+    
+    public static final String INVALID_NUMBER_FORMAT = "Neispravan format broja.";
+    public static final String KEY_SIZE_TOO_SMALL = "Kljuc mora biti bar 512 bita.";
+    
+}
