a52fe0defe1ec35f55c521b46c613a5d508d6b63
uros <uros@uros-velocity-micro>
2016-05-21 00:03:12 +0200

    Otvaranje KeyStore-a iz fajla.

--- zp-projekat/src/crypto/CertManagerImpl.java
+++ zp-projekat/src/crypto/CertManagerImpl.java
@@ -1,9 +1,16 @@
 package crypto;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 
 /**
  * CertManager interface implementation.
@@ -13,6 +20,70 @@ import java.security.SecureRandom;
 public class CertManagerImpl implements CertManager {
     
     private static String ENCRYPTION_ALGORITHM = "RSA";
+    private static String KEYSTORE_FILE = "my_keystore";
+    private static String KEYSTORE_TYPE = "pkcs12";
+    private static String KEYSTORE_PASSWORD = "pista";
+    
+    private KeyStore keyStore;
+    
+    public CertManagerImpl() {
+        // Initializes the key store.
+        try {
+            keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
+        } catch (KeyStoreException e) {
+            System.err.println(Errors.KEY_STORE_ERROR + " " + e.toString());
+            System.exit(Errors.KEY_STORE_ERROR_CODE);
+        }
+        
+        // Loads the key store from file, or creates a new key store file, if file doesn't exist.
+        try {
+            loadKeyStoreFromFile();
+        } catch (FileNotFoundException e) {  // Key store file not found.
+            try { // Attempts to create the key store file.
+                createNewKeyStore();
+                loadKeyStoreFromFile();
+            } catch (FileNotFoundException eNested) {  // Key store file still not found, error.
+                System.err.println(Errors.KEY_STORE_FILE_ERROR + " " + eNested.toString());
+                System.exit(Errors.KEY_STORE_FILE_ERROR_CODE);
+            } catch (IOException eNested) {  // Key Store file cannot be opened, error.
+                System.err.println(Errors.KEY_STORE_FILE_ERROR + " " + eNested.toString());
+                System.exit(Errors.KEY_STORE_FILE_ERROR_CODE);
+            }
+        } catch (IOException e) {  // Key store file cannot be opened, error.
+            System.err.println(Errors.KEY_STORE_FILE_ERROR + " " + e.toString());
+            System.exit(Errors.KEY_STORE_FILE_ERROR_CODE);
+        }
+    }
+    
+    private void loadKeyStoreFromFile() throws FileNotFoundException, IOException {
+        FileInputStream inputStream = new FileInputStream(KEYSTORE_FILE);
+        try {
+            keyStore.load(inputStream, KEYSTORE_PASSWORD.toCharArray());
+        } catch (CertificateException | NoSuchAlgorithmException e) {
+            System.err.println(Errors.KEY_STORE_ERROR + " " + e.toString());
+            System.exit(Errors.KEY_STORE_ERROR_CODE);
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+    
+    private void createNewKeyStore() throws FileNotFoundException, IOException {
+        FileOutputStream outputStream = new FileOutputStream(KEYSTORE_FILE);
+        try {
+            keyStore.load(null, KEYSTORE_PASSWORD.toCharArray());
+            keyStore.store(outputStream, KEYSTORE_PASSWORD.toCharArray());
+        }
+        catch (CertificateException | NoSuchAlgorithmException | KeyStoreException e) {
+            System.err.println(Errors.KEY_STORE_ERROR + " " + e.toString());
+            System.exit(Errors.KEY_STORE_ERROR_CODE);
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
     
     @Override
     public KeyPair generateKeyPair(int keySize) {
