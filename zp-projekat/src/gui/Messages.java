6c2c6c4e67e62c31c0eb132d648c7f2fbe80ca84
Dimitrije Petrovic <mitap94@yahoo.com>
2016-05-21 11:18:24 +0200

    dodate statusne poruke i boja

---
+++ zp-projekat/src/gui/Messages.java
@@ -0,0 +1,12 @@
+package gui;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Mita
+ */
+public class Messages {
+    public static final Color COLOR = Color.black;
+    public static final String SUCCESSFUL_KEY_GENERATION = "Uspesno generisan par kljuceva!";  
+}
