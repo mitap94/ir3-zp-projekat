4729e703af286a0f72d918f24f1271a0f1404489
uros <uros@uros-velocity-micro>
2016-05-25 18:22:28 +0200

    Refaktorovao inicijalizaciju CertManager-a.

--- zp-projekat/src/crypto/CertManager.java
+++ zp-projekat/src/crypto/CertManager.java
@@ -8,6 +8,7 @@ import java.security.KeyPair;
  */
 public interface CertManager {
     
+    void init();  // Initializes the CertManager implementation.
     KeyPair generateKeyPair(int keySize);  // Generates private-public key pair. Key size in bits.
     void importKeyPair(String path, String name);  // Imports key pair from file specified by path.
     void exportKeyPair(String path);  // Exports key pair to file specified by path.
