83ed759931f4e7bec6d004f7e6044c07c25b4f88
uros <uros@uros-velocity-micro>
2016-05-25 18:27:11 +0200

    Refaktorovao inicijalizaciju CertManager-a #2.

--- zp-projekat/src/crypto/CertManagerImpl.java
+++ zp-projekat/src/crypto/CertManagerImpl.java
@@ -21,9 +21,7 @@ import java.security.cert.CertificateException;
 public class CertManagerImpl implements CertManager {
     
     private static String ENCRYPTION_ALGORITHM = "RSA";
-    private static String KEYSTORE_FILE = "my_keystore";
     private static String KEYSTORE_TYPE = "pkcs12";
-    private static String KEYSTORE_PASSWORD = "pista";
     
     private String keyStoreFilePath, keyStorePassword;
     private KeyStore keyStore;
@@ -64,9 +62,9 @@ public class CertManagerImpl implements CertManager {
     }
     
     private void loadKeyStoreFromFile() throws FileNotFoundException, IOException {
-        FileInputStream inputStream = new FileInputStream(KEYSTORE_FILE);
+        FileInputStream inputStream = new FileInputStream(keyStoreFilePath);
         try {
-            keyStore.load(inputStream, KEYSTORE_PASSWORD.toCharArray());
+            keyStore.load(inputStream, keyStorePassword.toCharArray());
         } catch (CertificateException | NoSuchAlgorithmException e) {
             System.err.println(Errors.KEY_STORE_ERROR + " " + e.toString());
             System.exit(Errors.KEY_STORE_ERROR_CODE);
@@ -78,10 +76,10 @@ public class CertManagerImpl implements CertManager {
     }
     
     private void createNewKeyStore() throws FileNotFoundException, IOException {
-        FileOutputStream outputStream = new FileOutputStream(KEYSTORE_FILE);
+        FileOutputStream outputStream = new FileOutputStream(keyStoreFilePath);
         try {
-            keyStore.load(null, KEYSTORE_PASSWORD.toCharArray());
-            keyStore.store(outputStream, KEYSTORE_PASSWORD.toCharArray());
+            keyStore.load(null, keyStorePassword.toCharArray());
+            keyStore.store(outputStream, keyStorePassword.toCharArray());
         }
         catch (CertificateException | NoSuchAlgorithmException | KeyStoreException e) {
             System.err.println(Errors.KEY_STORE_ERROR + " " + e.toString());
