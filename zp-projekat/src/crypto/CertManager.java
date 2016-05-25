7498d149ee60b1f129dc1562ca368eee413e0dd8
uros <uros@uros-velocity-micro>
2016-05-25 20:18:35 +0200

    Ponovni refaktor za export key pair.

--- zp-projekat/src/crypto/CertManager.java
+++ zp-projekat/src/crypto/CertManager.java
@@ -11,9 +11,9 @@ public interface CertManager {
     
     void init();  // Initializes the CertManager implementation.
     KeyPair generateKeyPair(int keySize);  // Generates private-public key pair. Key size in bits.
-    void importKeyPair(String path, String name);  // Imports key pair from file specified by path.
-    void exportKeyPair(String filePath, String filePassword, String alias, String aliasPassword,
-            KeyStore keyPair, boolean aesEncrypted, String aesPassword);
-            // Exports key pair to file specified by path.
+    void importSertificate(String path, String name);  // Imports key pair from file specified by path.
+    void exportCertificate(String filePath, String filePassword, KeyStore keyPair,
+            boolean aesEncrypted, String aesPassword);
+            // Exports key pair (certificate) to file specified by path.
     
 }
