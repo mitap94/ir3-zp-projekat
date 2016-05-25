c1b91539a2c509af7d037f90c305dfdf749458d1
uros <uros@uros-velocity-micro>
2016-05-25 19:58:35 +0200

    Refaktorovan interfejs za eksport kljuceva/sertifikata.

--- zp-projekat/src/crypto/CertManager.java
+++ zp-projekat/src/crypto/CertManager.java
@@ -1,6 +1,7 @@
 package crypto;
 
 import java.security.KeyPair;
+import java.security.KeyStore;
 
 /**
  *
@@ -11,6 +12,8 @@ public interface CertManager {
     void init();  // Initializes the CertManager implementation.
     KeyPair generateKeyPair(int keySize);  // Generates private-public key pair. Key size in bits.
     void importKeyPair(String path, String name);  // Imports key pair from file specified by path.
-    void exportKeyPair(String path);  // Exports key pair to file specified by path.
+    void exportKeyPair(String filePath, String filePassword, String alias, String aliasPassword,
+            KeyStore keyPair, boolean aesEncrypted, String aesPassword);
+            // Exports key pair to file specified by path.
     
 }
