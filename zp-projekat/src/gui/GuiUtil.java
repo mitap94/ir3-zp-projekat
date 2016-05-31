34e0c4bb0ca47a59fdf2f0212cb2d9e7a99323c2
uros <uros@uros-velocity-micro>
2016-05-31 03:22:26 +0200

    Ubacio tekstualno formatiranje ekstenzija sertifikata.

--- zp-projekat/src/gui/GuiUtil.java
+++ zp-projekat/src/gui/GuiUtil.java
@@ -6,8 +6,14 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
+import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JPopupMenu;
@@ -16,6 +22,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.text.DefaultEditorKit;
+import org.bouncycastle.asn1.x509.GeneralName;
 
 /**
  *
@@ -24,8 +31,121 @@ import javax.swing.text.DefaultEditorKit;
 public class GuiUtil {
     
     public static String getExtensionsToStr(X509Certificate certificate) {
-        // TODO
-        return null;
+        StringBuilder extToStringBuilder = new StringBuilder();
+        Set criticalExtensions = certificate.getCriticalExtensionOIDs();
+
+        try {
+            // Basic constrains
+            extToStringBuilder.append("Basic constrains\n");
+            if ((criticalExtensions != null) && (criticalExtensions.contains("2.5.29.19")))
+                extToStringBuilder.append("* Critical\n");
+            int basicConstraints = certificate.getBasicConstraints();
+            if (basicConstraints == -1) {
+                extToStringBuilder.append("* Not CA\n");
+            } else {
+                extToStringBuilder.append("* CA: " + basicConstraints + '\n');
+            }
+
+            // Key usage
+            extToStringBuilder.append("--------------------------\n");
+            extToStringBuilder.append("Key usage\n");
+            if ((criticalExtensions != null) && (criticalExtensions.contains("2.5.29.15")))
+                extToStringBuilder.append("* Critical\n");
+
+            boolean[] keyUsageBits = certificate.getKeyUsage();
+            if (keyUsageBits != null) {
+                for (int i = 0; i < keyUsageBits.length; i++) {
+                    if (keyUsageBits[i]) {
+                        switch (i) {
+                            case 0:
+                                extToStringBuilder.append("* Digital signature\n");
+                                break;
+                            case 1:
+                                extToStringBuilder.append("* Non repudiation\n");
+                                break;
+                            case 2:
+                                extToStringBuilder.append("* Key encipherment\n");
+                                break;
+                            case 3:
+                                extToStringBuilder.append("* Data encipherment\n");
+                                break;
+                            case 4:
+                                extToStringBuilder.append("* Key agreement\n");
+                                break;
+                            case 5:
+                                extToStringBuilder.append("* Key cert sign\n");
+                                break;
+                            case 6:
+                                extToStringBuilder.append("* cRL sign\n");
+                                break;
+                            case 7:
+                                extToStringBuilder.append("* Encipher only\n");
+                                break;
+                            case 8:
+                                extToStringBuilder.append("* Decipher only\n");
+                                break;
+                        }
+                    }
+                }
+            }
+
+            // Alternative issuer names.
+            extToStringBuilder.append("--------------------------\n");
+            extToStringBuilder.append("Alternative issuer names:\n");
+            if ((criticalExtensions != null) && (criticalExtensions.contains("2.5.29.18")))
+                extToStringBuilder.append("* Critical\n");
+
+            LinkedList issuerAltList = new LinkedList();
+            Collection c = null;
+
+            try {
+                c = certificate.getIssuerAlternativeNames();
+
+                if (c != null) {
+                    Iterator i = c.iterator();
+
+                    while (i.hasNext()) {
+                        List list = (List) i.next();
+                        int type = ((Integer) list.get(0)).intValue();
+                        String stringValue = (String) list.get(1);
+
+                        switch (type) {
+                            case GeneralName.otherName:
+                                extToStringBuilder.append("* General name: " + stringValue + "\n");
+                                break;
+                            case GeneralName.rfc822Name:
+                                extToStringBuilder.append("* RFC822 name: " + stringValue + "\n");
+                                break;
+                            case GeneralName.dNSName:
+                                extToStringBuilder.append("* DNS name: " + stringValue + "\n");
+                                break;
+                            case GeneralName.x400Address:
+                                extToStringBuilder.append("* x400 Address name: " + stringValue + "\n");
+                                break;
+                            case GeneralName.directoryName:
+                                extToStringBuilder.append("* Directory name: " + stringValue + "\n");
+                                break;
+                            case GeneralName.ediPartyName:
+                                extToStringBuilder.append("* Edit party name: " + stringValue + "\n");
+                                break;
+                            case GeneralName.uniformResourceIdentifier:
+                                extToStringBuilder.append("* Uniform resource identifier: " + stringValue + "\n");
+                                break;
+                            case GeneralName.iPAddress:
+                                extToStringBuilder.append("* IP address: " + stringValue + "\n");
+                                break;
+                            case GeneralName.registeredID:
+                                extToStringBuilder.append("* Registered ID: " + stringValue + "\n");
+                                break;
+                        }
+                    }
+                }
+            } catch (CertificateParsingException e) { }
+        } catch (Exception e) {
+            return "No extensions!";
+        }
+
+        return extToStringBuilder.toString();
     }
     
     
@@ -42,7 +162,8 @@ public class GuiUtil {
         }
 
         Action copy = new DefaultEditorKit.CopyAction();
-
+    
+        
         copy.putValue(Action.NAME, "Copy");
         copy.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control C"));
         popupMenu.add(copy);
